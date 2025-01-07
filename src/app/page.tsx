ae1ce1284be909adb8bb4856685f5a6759be5163
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-01-06 18:30:51 -0800

    made education section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import About from "@/components/about";
-import Education from "@/components/education";
+// import About from "@/components/about";
+// import Education from "@/components/education";
 import Landing from "@/components/landing";
-import News from "@/components/news";
-import Welcome from "@/components/welcome";
+// import News from "@/components/news";
+// import Welcome from "@/components/welcome";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="">
       <Landing />
-      <Welcome />
+      {/* <Welcome />
       <About />
       <Education />
-      <News />
+      <News /> */}
     </div>
   );
 };
--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="flex w-full justify-start gap-4 bg-red-200">
+      <div>Title</div>
+      <div>
+        Education
+        <p>lalalalalalalala</p>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
