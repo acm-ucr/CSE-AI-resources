f4899df6d9f5b1f987b4426677be6f9db71eddff
Briana Garcia <briana_garcia_@outlook.com>
2025-01-15 19:08:10 -0800

    fix format

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/public/UCRLogo.svg"
+import Logo from "@/public/UCRLogo.svg";
 import { items } from "@/data/nav";
 
 type Item = {
@@ -18,7 +18,8 @@ const Navigation = () => {
       <div className="flex justify-between p-6">
         <div className="pl-4 text-4xl font-medium">
           <Image src={Logo} alt="UCR Logo"></Image>
-          CSE AI</div>
+          CSE AI
+        </div>
         <div className="flex space-x-4 pr-4 text-2xl font-normal">
           {items.map((item: Item) => (
             <Link href={item.href}> {item.name} </Link>
