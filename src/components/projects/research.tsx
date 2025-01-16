eb0057c62ecf40721c52a321fc8d3afde9651b88
Yubin Zhen <yubinzhen@ucr-secure-30-10-13-169-5.wnet.ucr.edu>
2025-01-16 01:07:16 -0800

    added project&research section

---
+++ src/components/projects/research.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const research = () => {
+  const placeholderImages = [
+    "https://placehold.co/360x360/00378F/00378F.png",
+    "https://placehold.co/360x360/00378F/00378F.png",
+    "https://placehold.co/360x360/00378F/00378F.png",
+  ];
+
+  return (
+    <section>
+      <div className="mb-7 inline-block">
+        <h1 className="mb-2 text-5xl">Projects & Research</h1>
+        <div className="h-1 bg-[#FFB81C]"></div>
+      </div>
+
+      <p className="font-inter mb-16 max-w-[1260px] text-[25px] font-normal leading-[30.26px]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+
+      <div className="flex justify-around gap-20">
+        {placeholderImages.map((image, index) => (
+          <div key={index} className="h-[360px] w-[360px] text-center">
+            <img
+              src={image}
+              alt="Image placeholder with blue background"
+              className="w-full"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default research;
