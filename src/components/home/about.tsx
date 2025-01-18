28dd245589bf8bb8f0821723c875496376e00292
Terrance Wong <twongzhw@gmail.com>
2025-01-18 11:15:30 -0800

    Made About Us Component

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div className="font-inter flex-wrap">
+      <div className="relative">
+        <h1 className="text-[60px] font-bold">About Us</h1>
+        <div className="absolute left-80 top-1/2 h-1 w-9/12 bg-yellow-500"></div>
+      </div>
+      <p className="mt-6 text-[25px]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat.
+      </p>
+    </div>
+  );
 };
 
 export default About;
