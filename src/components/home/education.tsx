cf60d06f331b0e5d32c4f7878340c640e016a8a3
Isaiah School <isaiahalcaydeSCHOOL@ISayAWuts-Macbook-Pro.local>
2025-01-19 00:19:42 -0800

    Created education componenent with header and text

--- src/components/home/education.tsx
+++ src/components/home/education.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Education = () => {
-  return <div>Education</div>;
+  return (
+    <div className="space-y-3">
+      <div className="flex items-center space-y-4">
+        <h1 className="textleft my-10 ml-20 text-6xl">Education</h1>
+        <div className="my-8 ml-4 mr-20 h-px flex-grow border-2 border-[#FFB81C]"></div>
+      </div>
+
+      <p className="m-20 text-left">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        labore vitae, molestias autem nam officiis numquam quam repudiandae
+        pariatur dolorem provident dolorum enim tempora doloribus officia
+        veritatis ad cupiditate id? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Assumenda labore vitae, molestias autem nam officiis
+        numquam quam repudiandae pariatur dolorem provident dolorum enim tempora
+        doloribus officia veritatis ad cupiditate id? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Assumenda labore vitae, molestias autem
+        nam officiis numquam quam repudiandae pariatur dolorem provident dolorum
+        enim tempora doloribus officia veritatis ad cupiditate id?
+      </p>
+    </div>
+  );
 };
 
 export default Education;
