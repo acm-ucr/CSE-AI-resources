046f0a51dada800ea6e67d83edf22b213c3aac79
BananazrCRAZY <Bananamandoge@gmail.com>
2025-01-20 13:54:50 -0800

    news layout updated

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,27 +1,27 @@
 interface News {
-    image: string;
-    refLink: string;
-    title: string;
-    text: string;
+  image: string;
+  refLink: string;
+  title: string;
+  text: string;
 }
 
 export const newsData: News[] = [
-    {
-        image: 'image',
-        refLink: 'link',
-        title: 'TITLE',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    },
-    {
-        image: 'image',
-        refLink: 'link',
-        title: 'TITLE',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    },
-    {
-        image: 'image',
-        refLink: 'link',
-        title: 'TITLE',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    }
+  {
+    image: "image",
+    refLink: "link",
+    title: "TITLE",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    image: "image",
+    refLink: "link",
+    title: "TITLE",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    image: "image",
+    refLink: "link",
+    title: "TITLE",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
 ];
