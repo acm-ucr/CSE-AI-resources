7247200c5bebb93e41d9320586375d9312de0507
BananazrCRAZY <Bananamandoge@gmail.com>
2025-01-20 13:07:45 -0800

    news.ts outline created

---
+++ src/data/news.ts
@@ -0,0 +1,23 @@
+interface News {
+    image: string;
+    title: string;
+    text: string;
+}
+
+export const newsData: News[] = [
+    {
+        image: 'imgLink',
+        title: 'TITLE',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        image: 'imgLink',
+        title: 'TITLE',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        image: 'imgLink',
+        title: 'TITLE',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+];
