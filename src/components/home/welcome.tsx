b72cedcd03cb72a9dbc62ad63078a6f39b94028c
Nathan Ramirez <nater0826@gmail.com>
2025-01-20 19:47:09 -0800

    Added welcome component

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Welcome = () => {
-  return <div>welcome</div>;
+  return (
+    <div className="flex flex-col items-start justify-start py-16 bg-white">
+      {/* Welcome Heading */}
+      <h1 className="text-4xl text-black mb-2">Welcome</h1>
+      {/* Yellow Underline */}
+      <div className="w-44 h-1 bg-ucr-orangeYellow mb-4"></div>
+      {/* Description */}
+      <p className="text-center text-black max-w-fit">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+  );
 };
-
+ 
 export default Welcome;
