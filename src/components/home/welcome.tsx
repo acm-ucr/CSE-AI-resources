5c7293721f2053ab3cb19eea56a1702889a6d566
Nathan Ramirez <nater0826@gmail.com>
2025-01-20 20:01:50 -0800

    added formatting to welcome.tsx

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Welcome = () => {
   return (
-    <div className="flex flex-col items-start justify-start py-16 bg-white">
+    <div className="flex flex-col items-start justify-start bg-white py-16">
       {/* Welcome Heading */}
-      <h1 className="text-4xl text-black mb-2">Welcome</h1>
+      <h1 className="mb-2 text-4xl text-black">Welcome</h1>
       {/* Yellow Underline */}
-      <div className="w-44 h-1 bg-ucr-orangeYellow mb-4"></div>
+      <div className="mb-4 h-1 w-44 bg-ucr-orangeYellow"></div>
       {/* Description */}
-      <p className="text-center text-black max-w-fit">
+      <p className="max-w-fit text-center text-black">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -17,5 +17,5 @@ const Welcome = () => {
     </div>
   );
 };
- 
+
 export default Welcome;
