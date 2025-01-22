2be7aae75f1577d27101d4b7823af7bff2c53d2d
ramirez-nathan <nater0826@gmail.com>
2025-01-22 11:57:51 -0800

    got rid of comments

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -3,11 +3,8 @@ import React from "react";
 const Welcome = () => {
   return (
     <div className="flex flex-col items-start justify-start bg-white py-16 pl-[105px] pr-[130px]">
-      {/* Welcome Heading */}
       <h1 className="mb-3 text-6xl text-black">Welcome</h1>
-      {/* Yellow Underline */}
       <div className="mb-9 h-1 w-72 bg-ucr-orangeYellow"></div>
-      {/* Description */}
       <p className="text-start text-2xl text-black">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
