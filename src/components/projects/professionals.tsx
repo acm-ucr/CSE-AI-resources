9ab61feaa4fc0b4136045091aef9a64dd89d8a2e
Raiden-Griff <185140831+Raiden-Griff@users.noreply.github.com>
2025-01-21 21:43:37 -0800

    Trying to correct the formatting issue

--- src/components/projects/professionals.tsx
+++ src/components/projects/professionals.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 
 const Professionals = () => {
+  return (
+    <div>
+      <h1>Engineers/Technical Professionals</h1>
+      <div className="yellow-line"></div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
 
-    return (
-        <div>
-          <h1>Engineers/Technical Professionals</h1>
-          <div className="yellow-line"></div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-          </p>
-          
-          <style>
-            {`
+      <style>
+        {`
               h1 {
                 font-family: "Arial", sans-serif;
                 font-size: 25px;
@@ -46,9 +45,9 @@ const Professionals = () => {
                 word-wrap: break-word;
               }
             `}
-          </style>
-        </div>
-      );
-    };
-    
-    export default Professionals;
+      </style>
+    </div>
+  );
+};
+
+export default Professionals;
