f78f7e9e3396ce143d514fa1cf4f8fdf7a2ff6a7
Shreyas Nallapareddy <snallapa1@gmail.com>
2025-01-21 17:17:05 -0800

    Basic External Resources, need to fix bar.

--- src/components/resources/external.tsx
+++ src/components/resources/external.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const external = () => {
   return (
     <div style={{ textAlign: "left", marginBottom: "20px" }}>
-      <h1 style={{ fontSize: "24px", fontWeight: "bold", margin: "0" }}>
+      <h1 style={{ fontSize: "60px", fontWeight: "400", margin: "0" }}>
         External Resources
       </h1>
       <div
@@ -14,7 +14,12 @@ const external = () => {
           margin: "5px auto",
         }}
       ></div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p style={{ fontSize: "25px", fontWeight: "400" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
     </div>
   );
 };
