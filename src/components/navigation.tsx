c8de2a25c18f09619132aad334538069bac6a7ad
Briana Garcia <briana_garcia_@outlook.com>
2025-01-21 16:13:49 -0800

    Small fixes + mapping

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -11,20 +11,23 @@ type Item = {
 
 const Navigation = () => {
   return (
-    <div className="flex-col bg-ucr-blue text-white">
+    <div className="bg-ucr-blue text-white">
       <p className="bg-ucr-yellow p-3 text-center text-lg font-light text-black">
         University of California, Riverside
       </p>
 
       <div className="flex h-24 justify-between">
         <div className="flex items-center space-x-3 pl-4 text-4xl font-medium">
-          <Image src={Logo} alt="UCR Logo"></Image>
+          <Image src={Logo} alt="UCR Logo"/>
           <p className="pr-4 text-5xl font-extralight text-ucr-yellow">|</p>
           CSE AI
         </div>
         <div className="flex items-center space-x-7 pr-4 text-2xl">
-          {items.map((item: Item) => (
-            <Link href={item.href}> {item.name} </Link>
+          {items.map((item: Item, index: number) => (
+            <Link href={item.href} key={index}>
+              {" "}
+              {item.name}{" "}
+            </Link>
           ))}
         </div>
       </div>
