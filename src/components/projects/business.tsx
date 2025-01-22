df257f3cf14bd6cb17ba9b8466345425c9aab308
Rachel Chan <rchan2023@gmail.com>
2025-01-21 16:19:52 -0800

    created business component

---
+++ src/components/projects/business.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Business = () => {
+  return (
+    <div className="items-left flex flex-col px-[5%]">
+      <div className="flex items-center space-x-5">
+        <h1 className="font-inter max-w-[1260px] text-4xl font-normal leading-relaxed">
+          Business
+        </h1>
+        <div className="h-0.5 w-10/12 bg-ucr-yellow pl-8"></div>
+      </div>
+      <p className="font-inter text-25px mb-10 p-5 font-normal leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+  );
+};
+
+export default Business;
