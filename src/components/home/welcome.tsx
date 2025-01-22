6825d4d4ad85f7d70386e8e17487440a1e3a2ec7
Nathan Ramirez <nater0826@gmail.com>
2025-01-21 21:04:41 -0800

    adjusted welcome component title size & text font

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Welcome = () => {
   return (
-    <div className="flex flex-col items-start justify-start bg-white py-16">
+    <div className="flex flex-col items-start justify-start bg-white py-16 pl-[105px] pr-[223px]">
       {/* Welcome Heading */}
-      <h1 className="mb-2 text-4xl text-black">Welcome</h1>
+      <h1 className="mb-3 text-6xl text-black">Welcome</h1>
       {/* Yellow Underline */}
-      <div className="mb-4 h-1 w-44 bg-ucr-orangeYellow"></div>
+      <div className="mb-9 h-1 w-72 bg-ucr-orangeYellow"></div>
       {/* Description */}
-      <p className="max-w-fit text-center text-black">
+      <p className="text-start text-2xl text-black">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
