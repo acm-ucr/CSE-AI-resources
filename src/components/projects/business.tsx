01442ea8b978a2e51f44df706d36041369038fc5
Rachel Chan <rchan2023@gmail.com>
2025-01-24 14:45:01 -0800

    update business component

--- src/components/projects/business.tsx
+++ src/components/projects/business.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Business = () => {
   return (
-    <div className="items-left flex flex-col px-[5%]">
-      <div className="flex items-center space-x-5">
-        <h1 className="font-inter max-w-[1260px] text-4xl font-normal leading-relaxed">
+    <div className="items-left flex flex-col p-20">
+      <div className="flex items-center">
+        <h1 className="font-inter py-10 text-6xl font-normal leading-relaxed">
           Business
         </h1>
-        <div className="h-0.5 w-10/12 bg-ucr-yellow pl-8"></div>
+        <div className="ml-8 h-1 w-11/12 bg-ucr-yellow"></div>
       </div>
-      <p className="font-inter text-25px mb-10 p-5 font-normal leading-relaxed">
+      <p className="font-inter text-2xl font-normal leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
