6dfe02bf19258339b57e6816811ba75de7511a1f
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-01-24 17:32:06 -0800

    clean up + added metadata

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="font-inter flex-wrap">
-      <div className="relative mx-auto max-w-5xl px-4">
-        <h1 className="text-6xl font-bold">About Us</h1>
-        <div className="absolute left-80 top-1/2 h-1 w-4/6 bg-yellow-500"></div>
+    <div className="font-inter flex w-5/6 flex-col">
+      <div className="relative flex h-full items-center gap-6">
+        <h1 className="text-6xl">About Us</h1>
+        <p className="h-1 w-3/4 bg-yellow-500" />
       </div>
-      <p className="mx-auto mt-6 max-w-5xl px-4 text-2xl">
+      <p className="mx-auto mt-6 w-full pr-2 text-2xl">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
