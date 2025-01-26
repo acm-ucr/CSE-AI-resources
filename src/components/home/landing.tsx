9d2cfa9006caf6a9e038ff34feabde356cb5ab3c
Aaron-Argueta <aarona2600@gmail.com>
2025-01-25 21:36:44 -0800

    used Image tag

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 const Landing = () => {
   return (
-    <div className="flex h-[780px] w-full justify-center bg-[url('/ucr_background.png')] bg-cover bg-center bg-no-repeat text-white">
+    <div className="flex h-[780px] w-full justify-center text-white">
+      <div className="absolute h-[780px] w-full">
+        <Image
+          fill
+          style={{ objectFit: "cover" }}
+          src="/ucr_background.png"
+          alt="UCR Background"
+        />
+      </div>
       <div className="absolute h-[780px] w-full bg-gradient-to-r from-[#003ca581] to-[#003DA5] opacity-[87%]"></div>
       <div className="z-10 flex flex-col items-center justify-center">
         <p className="text-[80px]">Computer Science Education</p>
