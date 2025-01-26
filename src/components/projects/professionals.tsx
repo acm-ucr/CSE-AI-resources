18b2f7be7b751ea8974e8bb088cb3ef6d3d4fa0f
Raiden-Griff <185140831+Raiden-Griff@users.noreply.github.com>
2025-01-25 17:44:48 -0800

    Fixed formatting issue with prettier

--- src/components/projects/professionals.tsx
+++ src/components/projects/professionals.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const professionals = () => {
   return (
-    <div className="font-sans top-0 left-0 ml-4 mt-4">
-    <div className="flex items-center">
-      <h1 className="text-3xl font-semibold text-left mr-2">Engineers/Technical Professionals</h1>
-      <div className="h-0.5 w-56 bg-[#ffb81c]"></div>
-    </div>
+    <div className="left-0 top-0 ml-4 mt-4 font-sans">
+      <div className="flex items-center">
+        <h1 className="mr-2 text-left text-3xl font-semibold">
+          Engineers/Technical Professionals
+        </h1>
+        <div className="h-0.5 w-56 bg-[#ffb81c]"></div>
+      </div>
 
-  <p className="ml-0 mt-2 w-1/2 text-sm text-left leading-relaxed">
+      <p className="ml-0 mt-2 w-1/2 text-left text-sm leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
