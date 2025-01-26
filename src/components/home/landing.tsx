efd5d8cf1192e5ee2c819e2efc61ab42605e5ae4
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-01-25 22:35:40 -0800

    clean up

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import Background from "@/public/ucr_background.png";
 
 const Landing = () => {
   return (
     <div className="flex h-[780px] w-full justify-center text-white">
-      <div className="absolute h-[780px] w-full">
+      <div className="absolute h-full w-full">
         <Image
-          fill
-          style={{ objectFit: "cover" }}
-          src="/ucr_background.png"
+          className="h-full object-cover"
+          src={Background}
           alt="UCR Background"
         />
       </div>
-      <div className="absolute h-[780px] w-full bg-gradient-to-r from-[#003ca581] to-[#003DA5] opacity-[87%]"></div>
+      <div className="absolute h-full w-full bg-gradient-to-r from-[#003ca581] to-[#003DA5] opacity-[87%]"></div>
       <div className="z-10 flex flex-col items-center justify-center">
         <p className="text-[80px]">Computer Science Education</p>
-        <div className="mb-6 h-[3px] w-[176px] bg-[#FFB81C]"></div>
+        <div className="mb-6 h-1 w-44 bg-[#FFB81C]"></div>
         <p className="text-3xl">Artificial Intelligence</p>
       </div>
     </div>
