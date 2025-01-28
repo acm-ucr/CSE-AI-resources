6d45fee2919ec6087ba69d8120b3a0cce00a1390
Shreyas Nallapareddy <snallapa1@gmail.com>
2025-01-27 17:42:41 -0800

    work in progress

--- src/components/resources/external.tsx
+++ src/components/resources/external.tsx
@@ -4,7 +4,6 @@ const External = () => {
   return (
     <div className="text-left mb-5">
       <h1 className="text-6xl font-light m-0">External Resources</h1>
-      <div className="w-[50%] h-[5px] bg-[#FFB81C] mt-2"></div>
       <p className="text-xl font-light mt-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
