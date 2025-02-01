9bea14dcb22e8990bcf7f8f5ade7737ee108d494
Aaron-Argueta <aarona2600@gmail.com>
2025-01-31 16:46:07 -0800

    created medical component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,13 @@
 import Professionals from "@/components/projects/professionals";
 import Research from "@/components/projects/research";
+import Medical from "@/components/projects/medical";
 
 const page = () => {
   return (
     <div>
       <Research />
       <Professionals />
+      <Medical />
     </div>
   );
 };
