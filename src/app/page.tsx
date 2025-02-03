ea737327ca556fd408bdb18c838722c3930e0666
Yubin Zhen <yubinzhen@Yubins-MacBook-Pro.local>
2025-02-03 11:30:58 -0800

    merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Education from "@/components/home/education";
 import Landing from "@/components/home/landing";
 import News from "@/components/home/news";
 import Welcome from "@/components/home/welcome";
-import Notes from "@/components/resources/notes";
 
 const Home = () => {
   return (
@@ -12,8 +11,13 @@ const Home = () => {
       <Welcome />
       <About />
       <Education />
-      <News />
-      <Notes />
+      <News
+        title="TITLE"
+        pageLink="/"
+        imageLink="https://www.magnite.com/wp-content/uploads/2024/05/surface-xSiQBSq-I0M-unsplash.jpg"
+        imageAlt="alt test"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
     </div>
   );
 };
