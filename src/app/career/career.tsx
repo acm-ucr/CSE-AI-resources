9710efd454a0e60a346fa7b3db8e6d83481001c9
Raiden-Griff <185140831+Raiden-Griff@users.noreply.github.com>
2025-02-03 16:41:59 -0800

    Adding code to try to view on the website. Not complete yet.

---
+++ src/app/career/career.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const career = () => {
+  return (
+    <div className="mt-4 flex w-5/6 flex-col items-center font-sans">
+      <div className="flex w-full items-center">
+        <h1 className="w-full text-left text-4xl font-semibold">Career</h1>
+        <div className="h-1 w-full bg-ucr-yellow" />
+      </div>
+      <p className="mt-2 w-full text-left text-lg leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+  );
+};
+
+export default career;
