3e561f3034e39b93c853b3291c3d8ea3c1417749
Raiden-Griff <185140831+Raiden-Griff@users.noreply.github.com>
2025-02-03 17:11:44 -0800

    fixed some minior things

--- src/app/career/career.tsx
+++ src/app/career/career.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 
-const career = () => {
+const Career = () => {
   return (
-    <div className="mt-4 flex w-5/6 flex-col items-center font-sans">
+    <div className="mt-4 m-4 flex w-5/6 flex-col items-center font-sans">
       <div className="flex w-full items-center">
-        <h1 className="w-full text-left text-4xl font-semibold">Career</h1>
-        <div className="h-1 w-full bg-ucr-yellow" />
+        <h1 className="text-left text-4xl border-b-4 border-ucr-orangeYellow">
+        Career
+        </h1>
       </div>
-      <p className="mt-2 w-full text-left text-lg leading-relaxed">
+      <p className="mt-5 m-4 text-left text-lg leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
       </p>
-    </div>
+      </div>
   );
 };
 
-export default career;
+export default Career;
--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Career from "@/app/career/career";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+  <div>page
+
+  <Career />
+
+  </div>
+  );
 };
 
 export default page;
