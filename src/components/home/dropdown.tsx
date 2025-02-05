b35857a502f8303057cfc236160c8551a9699715
Briana Garcia <briana_garcia_@outlook.com>
2025-02-05 15:04:44 -0800

    Added animation to triangles and used shad cn for dropdown

--- src/components/home/dropdown.tsx
+++ src/components/home/dropdown.tsx
@@ -4,6 +4,11 @@ import Image from "next/image";
 import BlueTriangle from "@/public/bluetriangle.svg";
 import YellowTriangle from "@/public/yellowtriangle.svg";
 
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+const Collapsible = CollapsiblePrimitive.Root;
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+
 type Prerequisite = {
   name: string;
   indent: number;
@@ -28,72 +33,87 @@ const Dropdown = ({
 
   return (
     <div className="pb-9 pt-2">
-      <div
-        className="m-4 flex cursor-pointer gap-6 text-4xl"
-        onClick={() => setCourseOpen(!courseOpen)}
-      >
-        <Image src={YellowTriangle} alt="Yellow Triangle" />
-        <p>{header}</p>
-      </div>
-
-      <div
-        className={`overflow-hidden transition-all duration-500 ease-in-out ${courseOpen ? "opacity-100" : "max-h-0 opacity-0"}`}
-      >
-        <p className="pb-2 pl-14 text-2xl font-light italic leading-7">
-          {description}
-        </p>
-
-        <div
-          className="flex cursor-pointer gap-6 overflow-hidden pl-7 text-2xl"
-          onClick={() => setResourcesOpen(!resourcesOpen)}
-        >
-          <Image src={BlueTriangle} alt="Blue Triangle" />
-          Resources
-        </div>
-
-        <div
-          className={`transistion-all overflow-hidden pb-2 pl-20 text-xl duration-500 ease-in-out ${resourcesOpen ? "opacity-100" : "max-h-0 opacity-0"}`}
-        >
-          <div className="mt-2 space-y-5">
-            <p>
-              Course Materials: Provided by UCR instructors (lecture slides,
-              notes).
-            </p>
-            <div>
-              Books:
-              {books.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </div>
-            <div>
-              Practice Platforms:
-              {platforms.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </div>
+      <Collapsible open={courseOpen} onOpenChange={setCourseOpen}>
+        <CollapsibleTrigger asChild>
+          <div className="m-4 flex cursor-pointer gap-6 text-4xl">
+            <Image
+              src={YellowTriangle}
+              alt="Yellow Triangle"
+              className={`duration-300 ${courseOpen ? "rotate-90" : "rotate-0"}`}
+            />
+            {header}
           </div>
-        </div>
+        </CollapsibleTrigger>
 
-        <div
-          className="flex cursor-pointer gap-6 pl-7 text-2xl"
-          onClick={() => setPrerequisitesOpen(!prerequisitesOpen)}
-        >
-          <Image src={BlueTriangle} alt="Blue Triangle" />
-          Prerequisites
-        </div>
+        <CollapsibleContent>
+          <p className="pb-2 pl-14 text-2xl font-light italic leading-7">
+            {description}
+          </p>
 
-        <div
-          className={`overflow-hidden pl-20 text-xl transition-all duration-500 ease-in-out ${prerequisitesOpen ? "opacity-100" : "max-h-0 opacity-0"}`}
-        >
-          <div className="mt-2">
-            {prerequisites.map((item, index) => (
-              <li key={index} style={{ marginLeft: `${item.indent * 30}px` }}>
-                {item.name}
-              </li>
-            ))}
-          </div>
-        </div>
-      </div>
+          <Collapsible open={resourcesOpen} onOpenChange={setResourcesOpen}>
+            <CollapsibleTrigger asChild>
+              <div className="mb-3 flex cursor-pointer gap-6 pl-7 text-2xl">
+                <Image
+                  src={BlueTriangle}
+                  alt="Blue Triangle"
+                  className={`duration-300 ${resourcesOpen ? "rotate-0" : "-rotate-90"}`}
+                />
+                Resources
+              </div>
+            </CollapsibleTrigger>
+
+            <CollapsibleContent>
+              <div className="mt-2 space-y-5 pb-2 pl-20 text-xl">
+                <p>
+                  Course Materials: Provided by UCR instructors (lecture
+                  slides,notes).
+                </p>
+                <div>
+                  Books:
+                  {books.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </div>
+                <div>
+                  Practice Platforms:
+                  {platforms.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </div>
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+
+          <Collapsible
+            open={prerequisitesOpen}
+            onOpenChange={setPrerequisitesOpen}
+          >
+            <CollapsibleTrigger asChild>
+              <div className="flex cursor-pointer gap-6 pl-7 text-2xl">
+                <Image
+                  src={BlueTriangle}
+                  alt="Blue Triangle"
+                  className={`duration-300 ${prerequisitesOpen ? "rotate-0" : "-rotate-90"}`}
+                />
+                Prerequisites
+              </div>
+            </CollapsibleTrigger>
+
+            <CollapsibleContent>
+              <div className="mt-2 pl-20 text-xl duration-500">
+                {prerequisites.map((item, index) => (
+                  <li
+                    key={index}
+                    style={{ marginLeft: `${item.indent * 30}px` }}
+                  >
+                    {item.name}
+                  </li>
+                ))}
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        </CollapsibleContent>
+      </Collapsible>
     </div>
   );
 };
