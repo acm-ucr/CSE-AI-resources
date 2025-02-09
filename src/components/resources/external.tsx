cb4c4050d259886ec30debad23ba6a477a05f7fd
Yubin Zhen <yubinzhen@Yubins-MacBook-Pro.local>
2025-02-09 09:44:34 -0800

    fixed external resources

--- src/components/resources/external.tsx
+++ src/components/resources/external.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const External = () => {
   return (
-    <div className-="p-20">
-      <div className="inline-block">
+    <div className="p-20">
+      <div className="mb-7 inline-block">
         <h1 className="mb-4 text-6xl">External Resources</h1>
         <div className="h-1 bg-ucr-yellow"></div>
       </div>
-      <p className="font-inter mb-16 max-w-[1260px] text-[25px] font-normal leading-relaxed">
+      <p className="font-inter text-2xl font-normal leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
