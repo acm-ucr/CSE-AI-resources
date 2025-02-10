82d4db3fd9a7a71f1a66a4b6760557b7ee22f302
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-02-09 18:15:51 -0800

    add

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,14 @@
 import Professionals from "@/components/projects/professionals";
 import Research from "@/components/projects/research";
 import Medical from "@/components/projects/medical";
+import Business from "@/components/projects/business";
 
 const page = () => {
   return (
     <div>
       <Research />
       <Professionals />
+      <Business />
       <Medical />
     </div>
   );
