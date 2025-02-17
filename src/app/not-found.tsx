6d78a84c9b642ecc7a1da19397af79f500f052ea
Isaiah School <isaiahalcaydeSCHOOL@ISayAWuts-MBP.lan>
2025-02-16 21:03:29 -0800

    404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,21 @@
+import React from "react"
+import Link from "next/link";
+
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return(
+        <div className="my-48 text-center">
+          <h1 className= "text-7xl mb-4">404</h1>
+          <div className="w-64 mb-4 mx-auto flex-grow border-2 border-ucr-yellow"></div>
+          <div className="text-2xl mb-16">Page Not Found</div>
+
+          <Link href="/" className="w-96 mx-auto border-4 border-ucr-yellow bg-ucr-yellow text-3xl py-6 px-16 mt-11 rounded-full">
+            Back To Home
+          </Link>
+
+
+
+        </div>
+  );
 };
 
 export default NotFound;
