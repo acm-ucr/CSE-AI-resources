bdf0ede0b65cf3681e352e82351fefd15f3399b8
Isaiah School <isaiahalcaydeSCHOOL@ISayAWuts-MBP.lan>
2025-02-16 21:41:39 -0800

    404 Page with button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,20 @@
-import React from "react"
+import React from "react";
 import Link from "next/link";
 
 const NotFound = () => {
-  return(
-        <div className="my-48 text-center">
-          <h1 className= "text-7xl mb-4">404</h1>
-          <div className="w-64 mb-4 mx-auto flex-grow border-2 border-ucr-yellow"></div>
-          <div className="text-2xl mb-16">Page Not Found</div>
+  return (
+    <div className="my-48 text-center">
+      <h1 className="mb-4 text-7xl">404</h1>
+      <div className="mx-auto mb-4 w-64 flex-grow border-2 border-ucr-yellow"></div>
+      <div className="mb-16 text-2xl">Page Not Found</div>
 
-          <Link href="/" className="w-96 mx-auto border-4 border-ucr-yellow bg-ucr-yellow text-3xl py-6 px-16 mt-11 rounded-full">
-            Back To Home
-          </Link>
-
-
-
-        </div>
+      <Link
+        href="/"
+        className="mx-auto mt-11 w-96 rounded-full border-4 border-ucr-yellow bg-ucr-yellow px-16 py-6 text-3xl"
+      >
+        Back To Home
+      </Link>
+    </div>
   );
 };
 
