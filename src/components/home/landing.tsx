3aa8912b000112f07f70bb20854edf177e9ddc4c
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-02-18 00:43:21 -0800

    rm comments

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -5,7 +5,6 @@ import Background from "@/public/ucr_background.webp";
 const Landing = () => {
   return (
     <div className="flex h-[500px] w-full justify-center text-white sm:h-[780px]">
-      {/* Background image */}
       <div className="absolute h-[500px] w-full sm:h-full">
         <Image
           className="h-full object-cover"
@@ -13,15 +12,13 @@ const Landing = () => {
           alt="UCR Background"
         />
       </div>
-      {/* Blue overlay */}
       <div className="absolute h-[500px] w-full bg-gradient-to-r from-[#003ca581] to-[#003DA5] opacity-[87%] sm:h-full"></div>
-      {/* Content */}
       <div className="z-10 flex flex-col items-center justify-center">
-        <p className="text-[20px] sm:text-[40px] md:text-[50px] lg:text-[70px] xl:text-[80px]">
+        <p className="text-[30px] sm:text-[40px] md:text-[50px] lg:text-[70px] xl:text-[80px] text-center">
           Computer Science Education
         </p>
         <div className="mb-2 mt-1 h-[2px] w-28 bg-[#FFB81C] sm:mb-3 sm:h-[3px] sm:w-36 md:mb-4 lg:mb-6 lg:h-1 lg:w-44"></div>
-        <p className="text-sm sm:text-base md:text-xl lg:text-2xl xl:text-3xl">
+        <p className="text-base md:text-xl lg:text-2xl xl:text-3xl">
           Artificial Intelligence
         </p>
       </div>
