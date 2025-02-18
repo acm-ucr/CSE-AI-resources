052f4737aeede55d270b11fe040e9035fc4cad2f
Nathan Ramirez <nater0826@gmail.com>
2025-02-17 23:39:01 -0800

    added responsiveness for education

--- src/components/home/education.tsx
+++ src/components/home/education.tsx
@@ -1,11 +1,12 @@
 const Education = () => {
   return (
-    <div className="flex w-5/6 flex-col">
-      <div className="flex items-center gap-6">
-        <h1 className="my-8 text-left text-6xl">Education</h1>
+    <div className="flex w-full max-w-5xl flex-col px-4 md:px-8">
+      <div className="flex items-center gap-4 md:gap-6">
+        <h1 className="whitespace-nowrap text-4xl md:text-6xl">Education</h1>
         <div className="flex-grow border-2 border-ucr-yellow"></div>
       </div>
-      <p className="">
+
+      <p className="mt-4 text-base leading-relaxed md:text-lg">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
         labore vitae, molestias autem nam officiis numquam quam repudiandae
         pariatur dolorem provident dolorum enim tempora doloribus officia
