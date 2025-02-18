dae804e78d0c69a7478bf1a10910471283dfb8bf
Shreyas Nallapareddy <snallapa1@gmail.com>
2025-02-17 18:53:38 -0800

    added education tab and education file

---
+++ src/components/education/education.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div className="mb-5 text-left">
+    <h1 className="m-0 text-6xl font-light">Education</h1>
+    <div className="mb-4 h-1 w-[543.01px] border-t-[5px] border-yellow-500"></div>
+    <p className="mt-4 text-xl font-light">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </p>
+  </div>
+  );
+};
+
+export default Education;
