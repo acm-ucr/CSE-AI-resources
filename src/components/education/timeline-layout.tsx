ff28c5b78c143235c24f8f60779327b2fc988bc6
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-02-18 16:03:30 -0800

    clean up

--- src/components/education/timeline-layout.tsx
+++ src/components/education/timeline-layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   Timeline,
   TimelineItem,
@@ -8,9 +9,15 @@ import {
   TimelineHeader,
 } from "@/components/ui/timeline";
 import { undergradData, graduateData } from "@/data/timeline";
+import { TimelineItemType } from "@/types";
 import { useState } from "react";
 
-export const TimelineLayout = () => {
+export type TImelineProps = {
+  title: string;
+  data: TimelineItemType[];
+};
+
+const TimelineSection = ({ title, data }: TImelineProps) => {
   const [collapsedItems, setCollapsedItems] = useState<{
     [key: number]: boolean;
   }>({});
@@ -20,54 +27,37 @@ export const TimelineLayout = () => {
   };
 
   return (
-    <div className="flex gap-10 md:w-5/6">
-      <Timeline className="mt-8 w-full">
-        <div className="mb-4 text-center text-xl font-semibold md:mb-10 md:text-4xl">
-          Undergraduate
-        </div>
-        {undergradData.map((item) => (
-          <TimelineItem key={item.id}>
-            <TimelineHeader>
-              <TimelineTime
-                onClick={() => toggleCollapse(item.id)}
-                className="cursor-pointer bg-ucr-blue/80 p-0 text-base duration-300 hover:bg-ucr-blue/70 md:p-3 md:text-lg"
-              >
-                {item.time}
-              </TimelineTime>
-              <TimelineTitle>{item.title}</TimelineTitle>
-            </TimelineHeader>
-            {collapsedItems[item.id] && item.description && (
-              <TimelineDescription className="transition-all duration-300">
-                {item.description}
-              </TimelineDescription>
-            )}
-          </TimelineItem>
-        ))}
-      </Timeline>
+    <Timeline className="mt-8 w-full">
+      <div className="mb-4 text-center text-xl font-semibold md:mb-10 md:text-4xl">
+        {title}
+      </div>
+      {data.map((item) => (
+        <TimelineItem key={item.id}>
+          <TimelineHeader>
+            <TimelineTime
+              onClick={() => toggleCollapse(item.id)}
+              className="cursor-pointer bg-ucr-blue/80 p-0 text-base duration-300 hover:bg-ucr-blue/70 md:p-3 md:text-lg"
+            >
+              {item.time}
+            </TimelineTime>
+            <TimelineTitle>{item.title}</TimelineTitle>
+          </TimelineHeader>
+          {collapsedItems[item.id] && item.description && (
+            <TimelineDescription className="transition-all duration-300">
+              {item.description}
+            </TimelineDescription>
+          )}
+        </TimelineItem>
+      ))}
+    </Timeline>
+  );
+};
 
-      <Timeline className="mt-8 w-full">
-        <div className="mb-4 text-center text-xl font-semibold md:mb-10 md:text-4xl">
-          Graduate
-        </div>
-        {graduateData.map((item) => (
-          <TimelineItem key={item.id}>
-            <TimelineHeader>
-              <TimelineTime
-                onClick={() => toggleCollapse(item.id)}
-                className="cursor-pointer bg-ucr-blue/80 duration-300 hover:bg-ucr-blue/70"
-              >
-                {item.time}
-              </TimelineTime>
-              <TimelineTitle>{item.title}</TimelineTitle>
-            </TimelineHeader>
-            {collapsedItems[item.id] && item.description && (
-              <TimelineDescription className="transition-all duration-300">
-                {item.description}
-              </TimelineDescription>
-            )}
-          </TimelineItem>
-        ))}
-      </Timeline>
+export const TimelineLayout = () => {
+  return (
+    <div className="flex gap-10 md:w-5/6">
+      <TimelineSection title="Undergraduate" data={undergradData} />
+      <TimelineSection title="Graduate" data={graduateData} />
     </div>
   );
 };
