2a3273db22448d66ca0e83e877b07d8093f93a4e
Terrance Wong <twongzhw@gmail.com>
2025-02-23 18:35:31 -0800

    made responsive business

--- src/components/projects/business.tsx
+++ src/components/projects/business.tsx
@@ -3,9 +3,9 @@ import Title from "../title";
 
 const Business = () => {
   return (
-    <div className="flex w-11/12 flex-col">
+    <div className="flex w-11/12 flex-col mx-auto">
       <Title title={"Business"} underline />
-      <p className="font-inter text-2xl font-normal leading-relaxed">
+      <p className="font-inter text-base md:text-lg lg:text-2xl font-normal leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
