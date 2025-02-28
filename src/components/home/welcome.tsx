8dd4a4e4bd99f1696199206f58bafda7477bb9bb
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-02-27 18:07:07 -0800

    cool beans 🫘

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -4,7 +4,7 @@ import Title from "../title";
 const Welcome = () => {
   return (
     <div className="my-4 flex w-5/6 flex-col items-start justify-start bg-white md:w-5/6">
-      <Title title=" Our Vision " underline={false} />
+      <Title title="Our Vision" underline={false} />
       <p className="mt-1 text-start text-lg text-black">
         We envision a future where AI education at UCR empowers students and
         faculty to drive innovation, research, and ethical AI development.
--- src/components/projects/research.tsx
+++ src/components/projects/research.tsx
@@ -3,18 +3,18 @@ import Title from "../title";
 
 const Research = () => {
   return (
-    <div className="p~5 mb-10 ml-auto mr-auto mt-20 w-11/12">
+    <div className="my-10 flex w-11/12 flex-col items-start">
       <Title title="Projects & Research" underline={false} />
-      <p className="font-inter mb-16 text-2xl font-normal leading-relaxed">
+      <p className="font-inter mb-10 text-2xl font-normal leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
       </p>
-      <div className="flex justify-around gap-10">
-        <div className="h-24 w-96 bg-ucr-blue/80 md:h-96" />
-        <div className="h-24 w-96 bg-ucr-blue/80 md:h-96" />
-        <div className="h-24 w-96 bg-ucr-blue/80 md:h-96" />
+      <div className="flex gap-10">
+        <div className="h-24 w-80 bg-ucr-blue/80 md:h-80" />
+        <div className="h-24 w-80 bg-ucr-blue/80 md:h-80" />
+        <div className="h-24 w-80 bg-ucr-blue/80 md:h-80" />
       </div>
     </div>
   );
