8708cb1170d52f51b68d1ac6451267f67be51aa4
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-03-11 11:33:49 -0700

    nice

--- src/components/projects/research.tsx
+++ src/components/projects/research.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import Title from "../title";
+import Link from "next/link";
 
 const Research = () => {
   return (
-    <div className="mx-auto my-5 flex w-11/12 flex-col items-start">
-      <Title title="Projects & Research" underline={false} />
-      <p className="font-inter pt-8 text-base font-normal leading-relaxed md:text-lg lg:text-2xl">
-        At UCR, our AI projects and research initiatives explore groundbreaking
-        advancements that bridge the gap between theory and real-world
-        applications. Through collaborative innovation, we tackle challenges in
-        various fields, leveraging AI to drive solutions that impact industries,
-        communities, and the future of technology.
-      </p>
-      {/* <div className="flex justify-center gap-10">
+    <div className="mx-auto my-5 flex w-11/12 flex-col gap-3">
+      <div className="w-full">
+        <Title title="Faculty" underline={false} />
+        <Link
+          href={"https://raise.ucr.edu/people"}
+          className="font-inter pt-8 text-base font-semibold leading-relaxed hover:text-ucr-blue md:text-lg lg:text-2xl"
+        >
+          View more
+        </Link>
+        {/* <div className="flex justify-center gap-10">
         <div className="h-24 w-80 bg-ucr-blue/80 md:h-80" />
         <div className="h-24 w-80 bg-ucr-blue/80 md:h-80" />
         <div className="h-24 w-80 bg-ucr-blue/80 md:h-80" />
-      </div> */}
+        </div> */}
+      </div>
     </div>
   );
 };
